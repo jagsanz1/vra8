a43520045fff33ab0f0565b5cb4717f5f935e077
Jorge Garcia <jorgega@vmware.com>
2022-01-12 14:29:19 -0600

    commented

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -3,3 +3,19 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+terraform {
+  required_providers {
+    vra = {
+      source  = "local/vmware/vra"
+      version = ">= 0.3.9"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "vra" {
+  url           = var.url
+  refresh_token = var.refresh_token
+  insecure = true
+}
--- aws/rds/rds.tf
+++ aws/rds/rds.tf
@@ -8,12 +8,12 @@ resource "aws_db_instance" "rds_mysql" {
   engine_version = "8.0.23"
   instance_class = "db.t2.micro"
   port = "3306"
-  db_subnet_group_name = "default_terraform" #Optional, had to previously create this DB group in AWS 
+  #db_subnet_group_name = "default_terraform" #Optional, had to previously create this DB group in AWS 
   name = "myRDS_db"
   username = var.username
   password = var.password
   parameter_group_name = "rdsmysqlgroup" #had to create parameter group in aws previous to running this
-  availability_zone = "us-east-1a" #optional
+  #availability_zone = "us-east-1a" #optional
   skip_final_snapshot = true
 #values above are for aws free tier, vpc value comes from test env
 
